de72334bfccbab209ca2312bad4fcbfc73121dcc
andreamaluta <andreamaluta@gmail.com>
2025-09-04 13:12:43 +0200

    first commit

---
+++ src/main/java/org/lesson/java/best_of_the_year/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.lesson.java.best_of_the_year.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String getBestYear(@RequestParam(name = "name") String name, Model model) {
+
+        model.addAttribute("name", name);
+        return "getBestOfTheYear";
+    }
+
+}
